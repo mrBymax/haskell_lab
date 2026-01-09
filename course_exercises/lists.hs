e34cef6cd4bacdd6008aef5e1ed010ba6edf1716
mrBymax <livebymax@gmail.com>
2026-01-09 15:38:10 +0100

    feat: finished list exercises

--- course_exercises/lists.hs
+++ course_exercises/lists.hs
@@ -28,3 +28,38 @@ quicksort (x : xs) =
 -- minOdd :: [Int] -> [Int]
 -- take the first two of a sorted array containing only odd numbers
 minOdd xs = take 2 (quicksort (filter (\n -> n `mod` 2 == 1) xs))
+
+-- 5. Build a list of pairs of ints where the first element is preserved and the second element is the sum of the remaining
+-- elements in the list
+-- build [1,2,3,4] --> [[1,9],[2,7],[3,4],[4,0]]
+build :: [Int] -> [(Int, Int)]
+build [] = []
+build (x : xs) = (x, sum xs) : build xs
+
+build2 :: [Int] -> [(Int, Int)]
+build2 list = go list (sum list)
+  where
+    go :: [Int] -> Int -> [(Int, Int)]
+    go [] _ = []
+    go (x : xs) currentSum =
+      let remainingSum = currentSum - x
+       in (x, remainingSum) : go xs remainingSum
+
+-- 6. Build a list of pairs of ints where the first element is preserved and the second element is the sum of the remaining
+-- elements in the list from the left
+-- build [1,2,3,4] --> [[1,0],[2,1],[3,3],[4,6]]
+build3 :: [Int] -> [(Int, Int)]
+build3 list = go list 0
+  where
+    go :: [Int] -> Int -> [(Int, Int)]
+    go [] _ = []
+    go (x : xs) currentAcc =
+      let totalSum = currentAcc + x
+       in (x, currentAcc) : go xs totalSum
+
+-- 7. Modify a list in which the min element is subtracted to each element in the list
+-- shiftToZero [1,2,3,4] -> [0,1,2,3]
+shiftToZero :: [Int] -> [Int]
+shiftToZero list =
+  let min = minimum list
+   in [x - min | x <- list]
